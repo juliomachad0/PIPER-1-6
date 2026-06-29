6b703d32a5082870c01afbc03353ace9afbc433b
Julio Machado <julioromac@outlook.com>
2026-06-29 12:23:23 -0300

    MNT: Updating plots structure

---
+++ plots/init_plots.m
@@ -0,0 +1,9 @@
+%% PLOTAGEM DOS RESULTADOS
+% Plotar resultados do Modelo Não Linear
+% evalin('base', 'plot3d_voo');
+% Plotar resultados do Xplane
+% evalin('base', 'plot3d_voo_xplane');
+% Plotar resultados do DBN
+% evalin('base', 'plot3d_voo_DBN');
+% Plotar os 3 em um só - comparação
+evalin('base', 'plot_compare_all');
