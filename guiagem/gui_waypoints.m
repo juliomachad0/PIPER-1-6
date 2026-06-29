07de963c4fc18f84243b4e1a4a3941df0f845c47
Julio Machado <julioromac@outlook.com>
2026-06-29 12:22:57 -0300

    MNT: Updating gui and pltos structure

--- guiagem/gui_waypoints.m
+++ guiagem/gui_waypoints.m
@@ -344,17 +344,11 @@ function gui_waypoints()
                 pauseSim(1, GlobalSocket);
                 fprintf("Xplane: Simulação Pausada")
             end
+            pauseSim(1, GlobalSocket);
             %% Plotar resultados
             close all;
-            % Plotar resultados do Modelo Não Linear
-            % evalin('base', 'plot3d_voo');
-            % Plotar resultados do Xplane
-            evalin('base', 'plot3d_voo_xplane');
-            % Plotar resultados do DBN
-            % evalin('base', 'plot3d_voo_DBN');
-            % Plotar os 3 em um só - comparação
-            evalin('base', 'plot_compare_all');
-
+            % chamar o função de plot
+            evalin('base', 'init_plots');
             lblStatus.Text = 'Simulação concluída com sucesso!';
             lblStatus.FontColor = [0 0.5 0];
 
